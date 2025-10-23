ebc535c4d85bf1c64cc1e543534c73dd75521089
ezekiela <ezekielaharisoa825@gmail.com>
2025-10-23 08:09:54 +0300

    controller des message reussi mais il rest le reaction

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -19,11 +19,11 @@ export const createMessage= async(req , res)=>{
 export const dropMessage=async(req,res)=>{
     const id=req.params.id;
     try {
-        const supResult=await pool.query("DELETE FROM messages WHERE id=$1 RETURNING*",[id]);
-
-        if(supResult.rowCount===0){
-            return res.status(401).json("message introuvable");
-        }
+        const supResult=await pool.query("DELETE  FROM messages WHERE id=$1 RETURNING *",[id]);
+       
+        // if(supResult.rowCount===0){
+        //     return res.status(401).json("message introuvable");
+        // }
         res.status(200).json({
             message:"Message bien supprimer",
             mss:supResult.rows[0]
@@ -31,4 +31,33 @@ export const dropMessage=async(req,res)=>{
     } catch (error) {
         res.status(500).json("erreur interne du serveur" + error)
     }
+}
+
+export const editMessage=async(req,res)=>{
+      const id=req.params.id;
+      const {newContenue}=req.body;
+      try {
+        const editResult=await pool.query("UPDATE messages  SET contenue=$1 WHERE id=$2 RETURNING *",[newContenue,id]);
+        if(!newContenue)return res.status(404).json("le message ne doit pas etre vide ")
+        if(editResult.rowCount===0){
+            return res.status(401).json("message introuvable");
+        }
+        res.status(200).json({
+            message:"Modification reussi",
+            reultat:editResult.rows[0]
+
+        })
+      } catch (error) {
+        res.status(500).json("Erreur interne du serveur "+ error);
+      }
+
+}
+export const reagirMess= async(req, res)=>{
+    const id=req.params.id;
+    try {
+        
+    } catch (error) {
+        
+    }
+
 }
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
   const messRouter=express.Router();
 import { authMiddle } from '../middleware/auth.middleware.js';
-import { createMessage, dropMessage } from '../controllers/message.controller.js';
+import { createMessage, dropMessage, editMessage } from '../controllers/message.controller.js';
 
 messRouter.post("/sendMess/:id",authMiddle,createMessage);
 messRouter.delete("/delete/:id",authMiddle,dropMessage);
+messRouter.put("/update/:id",authMiddle,editMessage);
 
 export default messRouter;
